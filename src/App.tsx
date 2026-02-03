554e565a2688eac757714a847e562d414fc65dc3
sifytul <sifytul09@gmail.com>
2026-02-03 17:56:48 +0000

    add some text

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Hello from Sifytul</h1>
+      <h3>A bioFreak Coder</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
